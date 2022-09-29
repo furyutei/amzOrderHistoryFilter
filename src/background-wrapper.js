c60c93f84e104dac77bc8cb0cc91ba2e14f58c5d
furyu <furyutei@gmail.com>
2022-09-29 13:26:40 +0900

    background-wrapper追加(Service Workerでエラーが確認できない不具合への対策)

---
+++ src/background-wrapper.js
@@ -0,0 +1,10 @@
+// ■参考
+// https://github.com/furyutei/twMediaDownloader/issues/89#issuecomment-1261621380
+// https://groups.google.com/a/chromium.org/g/chromium-extensions/c/lLb3EJzjw0o
+
+try {
+    importScripts('js/background.js');
+}
+catch (error) {
+    console.log(error);
+}
